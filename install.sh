486b88adb2c83543083985f509ae5a86bcc5df30
stefan911 <danielstefan.halasz@gmail.com>
2022-03-07 11:21:38 +0200

    Added license in header of files

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
-#  __  __    _  _____ ____  _____  __     _    ____  __  __ ___ _   _ 
+# matrix-admin-panel
+# Copyright (C) 2022 Stefan9110
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  __  __    _  _____ ____  _____  __     _    ____  __  __ ___ _   _
 # |  \/  |  / \|_   _|  _ \|_ _\ \/ /    / \  |  _ \|  \/  |_ _| \ | |
 # | |\/| | / _ \ | | | |_) || | \  /    / _ \ | | | | |\/| || ||  \| |
 # | |  | |/ ___ \| | |  _ < | | /  \   / ___ \| |_| | |  | || || |\  |
 # |_|  |_/_/   \_\_| |_| \_\___/_/\_\ /_/   \_\____/|_|  |_|___|_| \_|
-#                    Install script - Made by Stefan9110
+#                  Install script - Made by Stefan9110
                                                                     
 # Sudo command; replace with doas if needed
 AS_ROOT="sudo"
